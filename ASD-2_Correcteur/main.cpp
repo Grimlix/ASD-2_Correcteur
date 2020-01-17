81a6d48c2e8f53aec99ea3fba377048a3d24f2f0
LucasGianinetti <49147622+LucasGianinetti@users.noreply.github.com>
2020-01-17 16:26:07 +0100

    Transformation en minuscule ET '

--- ASD-2_Correcteur/main.cpp
+++ ASD-2_Correcteur/main.cpp
@@ -7,6 +7,7 @@
 using namespace std;
 
 
+
 /**
  * Fill an ordered set with lines from a text file
  * @param set set to fill with lines
@@ -17,11 +18,25 @@ void fillUnorderedSet(std::unordered_set<string>& set,string file, char delim){
     string line;
 
     if(myDictionary.is_open()) {
+        //for each word in dictionary
         while (getline(myDictionary,line, delim)) {
 
+            //lowercaps the word
             transform(line.begin(), line.end(), line.begin(),
-                           [](unsigned char c){ return tolower(c); });
-            //line.erase(remove_if(line.begin(), line.end(), [](char c) { return !isalpha(c); } ), line.end());
+                      [](unsigned char c){ return tolower(c); });
+
+            //remove non alpha of the words but leave '\''
+            int i = 0;
+            int len = line.length();
+
+            while(i < len){
+                if(!isalpha(line[i]) && line[i] != '\''){
+                    line.erase(i,1);
+                    len--;
+                }else{
+                    i++;
+                }
+            }
 
             set.insert(line);
         }
@@ -30,6 +45,16 @@ void fillUnorderedSet(std::unordered_set<string>& set,string file, char delim){
         cout <<"Fail to open file : " << file << endl;
     }
 }
+
+bool isWordInSet(std::unordered_set<string>& set,string word){
+    for(string wordInSet: set){
+        if(!word.compare(wordInSet)){
+            return true;
+        }
+    }
+    return false;
+}
+
 
 int main() {
 
@@ -46,27 +71,47 @@ int main() {
     //Create a unordered_set and fill it with words from dictionary
     std::unordered_set<string> dictionary;
     string myDictionary = "dictionary.txt";
-    fillUnorderedSet(dictionary,myDictionary, '\n');
+    fillUnorderedSet(dictionary,myDictionary,'\n');
 
     for(string word : dictionary){
-        //cout << word << endl;
+        cout << word << endl;
     }
 
+    /*
+    ifstream myFile("input_wikipedia.txt");
+    string line;
+    int j = 0;
+    if(myFile.is_open()){
+        while(getline(myFile,line)){
+            j++;
+            cout << "fuck ASD " << endl;bool isWordInSet(std::unordered_set<string>& set,string word){
+    for(string wordInSet: set){
+        if(!word.compare(wordInSet)){
+            return true;
+        }
+    }
+    return false;
+}
+        }
+    }else{
+        cout << "Fail to op file : " << line << endl;
+    }
+*/
+
+
+
     cout << dictionary.size() << endl;
-    
+/*
     std::unordered_set<string> input_sh;
     string myInput_sh = "input_sh.txt";
     fillUnorderedSet(input_sh,myInput_sh, ' ');
 
     for(string word : dictionary){
-        //cout << word << endl;
+        cout << word << endl;
     }
-    
-    cout << input_sh.size() << endl;
-    
-    
-    
-    
-    
+
+    cout << input_sh.size() << endl;*/
+
+
     return 0;
 }
