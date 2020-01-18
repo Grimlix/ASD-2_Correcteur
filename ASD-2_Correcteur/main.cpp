c9af68dc1dfead69aaff6000ed278ff2008dfb79
LucasGianinetti <49147622+LucasGianinetti@users.noreply.github.com>
2020-01-18 16:48:43 +0100

    output file / timers

--- ASD-2_Correcteur/main.cpp
+++ ASD-2_Correcteur/main.cpp
@@ -5,7 +5,7 @@
 #include <algorithm>
 #include <regex>
 #include <vector>
-
+#include <chrono>
 #include "TST.h"
 #include "Dictionary.h"
 #include "correction.h"
@@ -19,25 +19,28 @@ using namespace std;
 
 int main() {
 
+    ////////////////////////////////////////////////////////////////////////////////////////
+    //                                                                                    //
+    //  We chose stl structure unordered_set because it has O(1) complexity               //
+    //  for reasearch. And we chose unordered_set instead of unordered_map                //
+    //  because unordered_map insert the element when a research doesn't find an element  //
+    //                                                                                    //
+    ////////////////////////////////////////////////////////////////////////////////////////
 
-    //////////////////////////////////////////////////////////////////
-    //                          FIRST PART                          //
-    //////////////////////////////////////////////////////////////////
-
-    //We chose stl structure unordered_set because it has O(1) complexity
-    //for reasearch. And we chose unordered_set instead of unordered_map
-    //because unordered_map insert the element when a research doesn't find an element
 
     bool TST_MODE = false;
 
     string myDictionary = "dictionary.txt";
-
+    //chrono
+    auto start = std::chrono::high_resolution_clock::now();
     Dictionary dictionary = Dictionary(myDictionary, TST_MODE);
-
+    auto afterDico = std::chrono::high_resolution_clock::now();
+    auto dicoDuration = std::chrono::duration_cast<std::chrono::milliseconds>(afterDico - start);
+    cout << "Time take for dictionary creation (ms) : " << dicoDuration.count() << endl;
     int motDansDictionnaire = 0;
     int motPasDansDictionnaire = 0;
 
-    fstream file("input_sh.txt");
+    fstream file("input_simple.txt");
     string line;
     vector<string> wrongWords;
     vector<string> corrections;
@@ -73,16 +76,21 @@ int main() {
         cout << "Fail to open file : " << "input_wiki" << endl;
     }
 
+    //for each wrong word generate corrections in vector<string>corrections
     for(string word : wrongWords){
         setCorrections(dictionary,word, corrections);
     }
 
+    //write corrections in output file
+    std::ofstream output_file("./output_corrections.txt");
+    std::ostream_iterator<std::string> output_iterator(output_file, "\n");
+    std::copy(corrections.begin(), corrections.end(), output_iterator);
 
-    for(string elem : corrections){
-        cout << elem << endl;
-    }
+    auto afterCorrections = std::chrono::high_resolution_clock::now();
+    auto correctionsDuration = std::chrono::duration_cast<std::chrono::milliseconds>(afterCorrections - afterDico);
 
-    cout << " nbr de mots faux : " << motPasDansDictionnaire;
+    cout << "Time take for corrections creation (ms) : " << correctionsDuration.count() << endl;
+
 
     return 0;
 }
