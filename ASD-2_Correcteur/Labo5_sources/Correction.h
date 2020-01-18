21e14d6f250b5b6f4e9e07367255318f342e994b
Nicolas Hungerbühler <31136697+Grimlix@users.noreply.github.com>
2020-01-18 19:45:09 +0100

    fonctionne + commenté

--- ASD-2_Correcteur/Labo5_sources/Correction.h
+++ ASD-2_Correcteur/Labo5_sources/Correction.h
@@ -1,6 +1,13 @@
-//
-// Created by lucas on 18.01.20.
-//
+/*
+ * File:   Correction.h
+ * Author: Gianinetti Lucas, Hungerbühler Nicolas, Wojciechowski Cassandre
+ * Created on 18. janvier 2020
+ *
+ * Description: Contains function to get corrections from an incorrect word with a specific dicitonary
+ *
+ *
+ *
+ */
 
 
 #ifndef ASDLABO5_CORRECTION_H
@@ -9,22 +16,19 @@
 
 #include "Dictionary.h"
 
-bool isWordInSet(Dictionary dico, string word) {
-    return (dico.find_contains(word));
-}
-
-void letterInExcess(const Dictionary& dico, string word, vector<string>& corrections, bool& hasCorrection){
+//If word is corresponding to a word in dictionary but with 1 more letter we had it to vector<string> corrections
+void letterInExcess(Dictionary& dico, string word, vector<string>& corrections){
     string check;
     for(int i = 0; i < word.length(); ++i){
         check = word.substr(0, i) + word.substr(i+1, word.length());
-        if(isWordInSet(dico,check)){
-            hasCorrection = true;
+        if(dico.find_contains(check)){
             corrections.push_back("1:"+check);
         }
     }
 }
 
-void letterMissing(const Dictionary& dico, string word, vector<string>& corrections, bool& hasCorrection){
+//If word is corresponding to a word in dictionary but with 1 missing letter we had it to vector<string> corrections
+void letterMissing(Dictionary& dico, string word, vector<string>& corrections){
     string check;
     const string alphabet = "abcdefghijklmnopqrstuvwxyz'";
     const unsigned size = 1;
@@ -33,8 +37,7 @@ void letterMissing(const Dictionary& dico, string word, vector<string>& correcti
             check = word;
             check.insert(i, size, alphabet[j]);
 
-            if(isWordInSet(dico, check)) {
-                hasCorrection = true;
+            if(dico.find_contains(check)) {
                 corrections.push_back("2:"+check);
 
             }
@@ -42,15 +45,15 @@ void letterMissing(const Dictionary& dico, string word, vector<string>& correcti
     }
 }
 
-void wrongLetter(const Dictionary& dico, string word, vector<string>& corrections, bool& hasCorrection){
+//If word is corresponding to a word in dictionary but with 1 misspelled letter we had it to vector<string> corrections
+void wrongLetter(Dictionary& dico, string word, vector<string>& corrections){
     string check = word;
     const string alphabet = "abcdefghijklmnopqrstuvwxyz'";
     for(int i = 0; i < word.length(); ++i){
         check = word;
         for(int j = 0; j < alphabet.length(); ++j){
             check[i] = alphabet[j];
-            if(isWordInSet(dico,check)){
-                hasCorrection = true;
+            if(dico.find_contains(check)){
                 corrections.push_back("3:"+check);
             }
         }
@@ -58,7 +61,8 @@ void wrongLetter(const Dictionary& dico, string word, vector<string>& correction
     }
 }
 
-void swappedLetter(const Dictionary& dico, string word, vector<string>& corrections, bool& hasCorrection){
+//If word is corresponding to a word in dictionary but with 2l letters in incorect order we had it to vector<string> corrections
+void swappedLetter(Dictionary& dico, string word, vector<string>& corrections){
 
     int len = word.length() -1;
     string temp = word;
@@ -67,21 +71,21 @@ void swappedLetter(const Dictionary& dico, string word, vector<string>& correcti
         char ctemp = temp[i];
         temp[i] = temp[i+1];
         temp[i+1] = ctemp;
-        if(isWordInSet(dico,temp)){
-            hasCorrection = true;
+        if(dico.find_contains(temp)){
             corrections.push_back("4:"+temp);
         }
     }
 }
 
-
-void setCorrections(const Dictionary& dico, string word, vector<string>& corrections){
-    bool hasCorrection = false;
+//get every correction for word and had it to corrections
+void setCorrections(Dictionary& dico, string word, vector<string>& corrections){
     corrections.push_back("*"+word);
-    letterInExcess(dico,word,corrections,hasCorrection);
-    letterMissing(dico,word,corrections,hasCorrection);
-    wrongLetter(dico,word,corrections, hasCorrection);
-    swappedLetter(dico,word,corrections,hasCorrection);
+    letterInExcess(dico,word,corrections);
+    letterMissing(dico,word,corrections);
+    wrongLetter(dico,word,corrections);
+    swappedLetter(dico,word,corrections);
+
+
 }
 
 #endif //ASDLABO5_CORRECTION_H
