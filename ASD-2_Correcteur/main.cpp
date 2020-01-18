1cc372939e29bf9088aabf837ff67f0f27ea7803
LucasGianinetti <49147622+LucasGianinetti@users.noreply.github.com>
2020-01-18 20:22:18 +0100

    main clean / commenté

--- ASD-2_Correcteur/main.cpp
+++ ASD-2_Correcteur/main.cpp
@@ -1,3 +1,17 @@
+/*
+ * File:   main.cpp
+ * Author: Gianinetti Lucas, Hungerbühler Nicolas, Wojciechowski Cassandre
+ * Created on 18. janvier 2020
+ *
+ * Description: File in which we test our dictionary corrector with 2 differents data structure and a test txt file
+ *
+ * Remark:We chose stl structure unordered_set because it has O(1) complexity
+ *        for reasearch. And we chose unordered_set instead of unordered_map
+ *        because unordered_map insert the element when a research doesn't find an element
+ *
+ *
+ */
+
 #include <iostream>
 #include <string>
 #include <unordered_set>
@@ -13,83 +27,58 @@
 
 using namespace std;
 
+// calculate execution time of corrections from a specific filename with a specific dictionary
+void createOutputCorrectionsFile(string filename, Dictionary& dictionary){
+    fstream file(filename);
+    vector<string> corrections;
 
+    string outputFilename = "out" + filename.substr(2,filename.length());
 
+    auto beforCorrections = std::chrono::high_resolution_clock::now();
+
+    generateCorrectionFromFile(file,corrections,dictionary);
+
+    std::ofstream output("./" + outputFilename);
+    std::ostream_iterator<std::string> output_iterator(output, "\n");
+    std::copy(corrections.begin(), corrections.end(), output_iterator);
+
+    auto afterCorrections = std::chrono::high_resolution_clock::now();
+    auto correctionDuration = std::chrono::duration_cast<std::chrono::microseconds>(afterCorrections - beforCorrections);
+
+    cout << "Time taken to generate corrections of " << filename << " into file " << outputFilename
+    << " : " << correctionDuration.count() << " [μs]" << endl;
+}
 
 
 int main() {
 
-    ////////////////////////////////////////////////////////////////////////////////////////
-    //                                                                                    //
-    //  We chose stl structure unordered_set because it has O(1) complexity               //
-    //  for reasearch. And we chose unordered_set instead of unordered_map                //
-    //  because unordered_map insert the element when a research doesn't find an element  //
-    //                                                                                    //
-    ////////////////////////////////////////////////////////////////////////////////////////
 
 
+    //boolean to chose which data structure we want to use for our dictionary
+    //true -> Ternary Search Tree
+    //false -> unordered_set<string>
     bool TST_MODE = false;
 
+    //File from which we take our words to implement dictionary
     string myDictionary = "dictionary.txt";
+
     //chrono
     auto start = std::chrono::high_resolution_clock::now();
+
+    //we create a wrapper for the data_structure
     Dictionary dictionary = Dictionary(myDictionary, TST_MODE);
+
     auto afterDico = std::chrono::high_resolution_clock::now();
-    auto dicoDuration = std::chrono::duration_cast<std::chrono::milliseconds>(afterDico - start);
-    cout << "Time take for dictionary creation (ms) : " << dicoDuration.count() << endl;
-    int motDansDictionnaire = 0;
-    int motPasDansDictionnaire = 0;
+    auto dicoDuration = std::chrono::duration_cast<std::chrono::microseconds>(afterDico - start);
 
-    fstream file("input_simple.txt");
-    string line;
-    vector<string> wrongWords;
-    vector<string> corrections;
+    cout << "Time taken to create dictionary : " << dicoDuration.count() << " [μs] "<< endl;
 
-    if (file.is_open()) {
-
-        std::regex words_regex("[a-z]+('[a-z]+)*");
-        auto words_end = std::sregex_iterator();
-        //for each word in dictionary
-        while (getline(file, line)) {
-            //lowercaps the word
-            transform(line.begin(), line.end(), line.begin(),
-                      [](unsigned char c) {
-                          return tolower(c); });
+    createOutputCorrectionsFile("input_wikipedia.txt",dictionary);
+    createOutputCorrectionsFile("input_sh.txt",dictionary);
+    createOutputCorrectionsFile("input_lates.txt",dictionary);
+    createOutputCorrectionsFile("input_simple.txt",dictionary);
 
 
-            auto line_begin = sregex_iterator(line.begin(), line.end(), words_regex);
-            auto line_end = sregex_iterator();
-
-            for(sregex_iterator i = line_begin; i != line_end; i++){
-                smatch match = *i;
-                if(isWordInSet(dictionary, match.str())){
-                    motDansDictionnaire++;
-                }else{
-                    motPasDansDictionnaire++;
-                    wrongWords.push_back(match.str());
-
-                }
-            }
-        }
-        file.close();
-    } else {
-        cout << "Fail to open file : " << "input_wiki" << endl;
-    }
-
-    //for each wrong word generate corrections in vector<string>corrections
-    for(string word : wrongWords){
-        setCorrections(dictionary,word, corrections);
-    }
-
-    //write corrections in output file
-    std::ofstream output_file("./output_corrections.txt");
-    std::ostream_iterator<std::string> output_iterator(output_file, "\n");
-    std::copy(corrections.begin(), corrections.end(), output_iterator);
-
-    auto afterCorrections = std::chrono::high_resolution_clock::now();
-    auto correctionsDuration = std::chrono::duration_cast<std::chrono::milliseconds>(afterCorrections - afterDico);
-
-    cout << "Time take for corrections creation (ms) : " << correctionsDuration.count() << endl;
 
 
     return 0;
